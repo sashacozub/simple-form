7e5410a268ec25d83a5c508066b8400bfd16463f
sashacozub <81567588+sashacozub@users.noreply.github.com>
2021-08-15 17:25:54 +0200

    Add info box with instructions

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ const country = document.querySelector('#country');
 const password = document.querySelector('#password');
 const passwordRepeat = document.querySelector('#password2');
 const submitButton = document.querySelector('#submit-btn');
-const approvedColor = '1px solid rgb(50, 240, 123)';
-const rejectedColor = '1px solid rgb(240, 88, 50)';
+
+const approvedColor = 'rgb(50, 240, 123)';
+const rejectedColor = 'rgb(240, 88, 50)';
+
+const firstNameInfoMin = document.querySelector('[data-fname="min"]');
+const firstNameInfoLetters = document.querySelector('[data-fname="letters"]');
+const firstNameInfoMax = document.querySelector('[data-fname="max"]');
+
+const lastNameInfoMin = document.querySelector('[data-lname="min"]');
+const lastNameInfoLetters = document.querySelector('[data-lname="letters"]');
+const lastNameInfoMax = document.querySelector('[data-lname="max"]');
+
+const emailInfo = document.querySelector('[data-email]');
+
+const countryInfo = document.querySelector('[data-country]');
+
+const passwordInfoMin = document.querySelector('[data-password="min"]');
+const passwordInfoChar = document.querySelector('[data-password="char"]');
+const passwordInfoMax = document.querySelector('[data-password="max"]');
+const passwordInfoMatch = document.querySelector('[data-password="match"]')
+
 
 // Populate "Country" field with all the options on page load
 window.addEventListener('load', () => getCountries());
@@ -49,6 +68,8 @@ lastName.addEventListener('keyup', () => checkLastName());
 
 email.addEventListener('keyup', () => checkEmail());
 
+country.addEventListener('change', () => checkCountry());
+
 password.addEventListener('keyup', () => checkPassword());
 
 passwordRepeat.addEventListener('keyup', () => checkPasswordRepeat());
@@ -56,74 +77,105 @@ passwordRepeat.addEventListener('keyup', () => checkPasswordRepeat());
 
 const checkFirstName = () => {
     const reg = new RegExp(/^[A-Za-z]+$/);
+
+    firstName.style.border = `1px solid ${approvedColor}`;
+    firstNameInfoMin.style.color = approvedColor;
+    firstNameInfoLetters.style.color = approvedColor;
+    firstNameInfoMax.style.color = approvedColor;
     
     if (firstName.value.length < 3) {
-        console.log('first name: minimum 3 letters')
-        firstName.style.border = rejectedColor;
-    } else if (!firstName.value.match(reg)) {
-        console.log('first name: letters only!')
-        firstName.style.border = rejectedColor;
-    } else if (firstName.value.length > 15) {
-        console.log('first name: must be under 15 characters')
-        firstName.style.border = rejectedColor;
-    } else {
-        firstName.style.border = approvedColor;
+        firstName.style.border = `1px solid ${rejectedColor}`;
+        firstNameInfoMin.style.color = rejectedColor;
+    }
+    
+    if (!firstName.value.match(reg)) {
+        firstName.style.border = `1px solid ${rejectedColor}`;
+        firstNameInfoLetters.style.color = rejectedColor;
+    }
+
+    if (firstName.value.length > 15) {
+        firstName.style.border = `1px solid ${rejectedColor}`;
+        firstNameInfoMax.style.color = rejectedColor;
     }
 };
 
 const checkLastName = () => {
     const reg = new RegExp(/^[A-Za-z]+$/);
 
+    lastName.style.border = `1px solid ${approvedColor}`;
+    lastNameInfoMin.style.color = approvedColor;
+    lastNameInfoLetters.style.color = approvedColor;
+    lastNameInfoMax.style.color = approvedColor;
+
     if (lastName.value.length < 3) {
-        console.log('last name: minimum 3 letters')
-        lastName.style.border = rejectedColor;
-    } else if (!lastName.value.match(reg)) {
-        console.log('last name: letters only!')
-        lastName.style.border = rejectedColor;
-    } else if (lastName.value.length > 15) {
-        console.log('last name: must be under 15 characters')
-        lastName.style.border = rejectedColor;
-    } else {
-        lastName.style.border = approvedColor;
+        lastName.style.border = `1px solid ${rejectedColor}`;
+        lastNameInfoMin.style.color = rejectedColor;
+    }
+    
+    if (!lastName.value.match(reg)) {
+        lastName.style.border = `1px solid ${rejectedColor}`;
+        lastNameInfoLetters.style.color = rejectedColor;
+    }
+
+    if (lastName.value.length > 15) {
+        lastName.style.border = `1px solid ${rejectedColor}`;
+        lastNameInfoMax.style.color = rejectedColor;
     }
 };
 
 const checkEmail = () => {
     const reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
+    email.style.border = `1px solid ${approvedColor}`;
+    emailInfo.style.color = approvedColor;
+
     if (!email.value.match(reg)) {
-        console.log('must be email')
-        email.style.border = rejectedColor;
-    } else {
-        email.style.border = approvedColor;
-    }
-};
-
-const checkPassword = () => {
-    const reg = new RegExp(/^(?=.*[a-z]).{6,16}$/);
-
-    if (!password.value.match(reg)) {
-        console.log('pass conditions here')
-        password.style.border = rejectedColor;
-    } else {
-        password.style.border = approvedColor;
-    }
-};
-
-const checkPasswordRepeat = () => {
-    if (passwordRepeat.value.trim() === '') {
-        console.log('repeat password')
-        passwordRepeat.style.border = rejectedColor;
-    } else if (passwordRepeat.value !== password.value) {
-        console.log('passwords do not match')
-        passwordRepeat.style.border = rejectedColor;
-    } else {
-        passwordRepeat.style.border = approvedColor;
+        email.style.border = `1px solid ${rejectedColor}`;
+        emailInfo.style.color = rejectedColor;
     }
 };
 
 const checkCountry = () => {
-    if (country.value === 'country') {
-        console.log('please select country')
+    country.style.border = `1px solid ${rejectedColor}`;
+    countryInfo.style.color = rejectedColor;
+
+    if (country.value !== 'country') {
+        countryInfo.style.color = approvedColor;
+        country.style.border = `1px solid ${approvedColor}`;
     };
 };
+
+const checkPassword = () => {
+    const reg = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{1,16}$/);
+
+    password.style.border = `1px solid ${approvedColor}`;
+    passwordInfoMin.style.color = approvedColor;
+    passwordInfoChar.style.color = approvedColor;
+    passwordInfoMax.style.color = approvedColor;
+
+    if (password.value.length < 6) {
+        password.style.border = `1px solid ${rejectedColor}`;
+        passwordInfoMin.style.color = rejectedColor;
+    }
+
+    if (!password.value.match(reg)) {
+        password.style.border = `1px solid ${rejectedColor}`;
+        passwordInfoChar.style.color = rejectedColor;
+    }
+
+    if (password.value.length > 16) {
+        password.style.border = `1px solid ${rejectedColor}`;
+        passwordInfoMax.style.color = rejectedColor;
+    }
+};
+
+const checkPasswordRepeat = () => {
+    passwordRepeat.style.border = `1px solid ${rejectedColor}`;
+    passwordInfoMatch.style.color = rejectedColor;
+
+    if (passwordRepeat.value === password.value) {
+        passwordRepeat.style.border = `1px solid ${approvedColor}`;
+        passwordInfoMatch.style.color = approvedColor;
+    }
+};
+
